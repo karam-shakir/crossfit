c5cd33a92c1d8341583b38e14edde88a19f11f16
karam-shakir <shakir.alreeshi@gmail.com>
2026-06-06 21:46:00 +0300

    fix: defer MONGODB_URI check to runtime not build time

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,31 +1,30 @@
 import { MongoClient, Db } from 'mongodb';
 
-const uri = process.env.MONGODB_URI as string;
 const dbName = process.env.MONGODB_DB || 'matanikeh';
-
-if (!uri) throw new Error('MONGODB_URI is not defined in environment variables');
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+function getClientPromise(): Promise<MongoClient> {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error('MONGODB_URI is not defined in environment variables');
+
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+      const client = new MongoClient(uri);
+      global._mongoClientPromise = client.connect();
+    }
+    return global._mongoClientPromise;
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
+
+  const client = new MongoClient(uri);
+  return client.connect();
 }
 
 export async function getDb(): Promise<Db> {
-  const c = await clientPromise;
-  return c.db(dbName);
+  const client = await getClientPromise();
+  return client.db(dbName);
 }
 
-export default clientPromise;
+export default { getDb };
